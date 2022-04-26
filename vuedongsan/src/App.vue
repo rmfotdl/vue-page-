010cd5267cafe095f9c5fd2928b4c20a5e2206d8
koosoyeon <hmkoosy@gmail.com>
2022-04-26 11:25:31 +0900

    export default 로 주고, import 작명 from "주소" 로 받고 !

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -12,33 +12,21 @@
   </div>
 
 
-  <div>
+  <div v-for="(contants,i) in 원룸들" :key="i">
     <h1>원룸샵</h1>
-    <img src="./assets/room1.png" alt="" class="room-img">
-    <h4 @click="모달창열렸니 = true">{{ products[0] }}원룸</h4>
-    <p>50만원</p>
-    <button @click="신고수[0]+=1">허위매물신고</button><span>신고수 : {{신고수[0]}}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.png" alt="" class="room-img">
-    <h4>{{ products[1] }}</h4>
-    <p>60만원</p>
-    <button @click="신고수[1]+=1">허위매물신고</button><span>신고수 : {{신고수[1]}}</span>
-  </div>
-  <div>
-    <img src="./assets/room3.png" alt="" class="room-img">
-    <h4>{{ products[2] }}</h4>
-    <p>70만원</p>
-    <button @click="신고수[2]+=1">허위매물신고</button><span>신고수 : {{신고수[2]}}</span>
+    <img :src="contants.image" alt="" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{contants.title }}</h4>
+    <p>{{ contants.price }}</p>
   </div>
 </template>
 
 <script>
- 
+import data from './assets/oneroom.js'
 export default {
   name: 'App',
   data(){
     return{
+      원룸들 : data,
       모달창열렸니 : false,
       신고수 : [0,0,0],
       menus : ['home', 'shop', 'about'],
